9c0191eb4ac075a303cbcc4cadccffd3d8557b1b
ugraveknight <45131023+ugraveknight@users.noreply.github.com>
2019-08-25 22:50:16 -0400

    1.4

---
+++ KillfeedMonitor.py
@@ -0,0 +1,106 @@
+from pyautogui import locateAll, center
+from glob import glob
+from itertools import cycle
+from time import sleep, time
+	
+def TreatSub(info,name,ListOfCycles,c_killfeed):
+	i = 0 if info == 'SubDeath'  else \
+	    1 if info == 'DomDeath'  else \
+	    2 if info == 'TeamDeath' else \
+	    3 if info == 'Assist' else \
+	    4 if info == 'Kill' else ''
+	try:
+		c_killfeed.send(next(ListOfCycles[i]))
+	except StopIteration:
+		pass
+
+def TrackDeath(name,TeamColor,Sub,Dom,ListOfCycles,c_killfeed):
+	print('Death:',TeamColor,name)
+	TreatStr = 'DomDeath'  if name == Dom.replace('Resources\\Killfeed\\Overwatch\\','').replace('.png','') and TeamColor == 'Blue' else \
+			   'SubDeath'  if name == Sub else \
+			   'TeamDeath' if TeamColor == 'Blue' else 'Kill'
+	print(TreatStr,TeamColor,name)
+	TreatSub(TreatStr,name,ListOfCycles,c_killfeed) if TreatStr != '' else 0
+	
+def GenTeamColor(im, x, y):
+	BorderRed,_,BorderBlue = im.getpixel((int(x), int(y)))
+	return 'Red' if BorderRed > BorderBlue else 'Blue'
+
+def Main(im,Files,Sub,Dom,Cords,ListOfCycles,c_killfeed,KFPath,Debug=False):
+	try:
+		newtime = time() if Debug==True else 0
+		for file in Files:
+			newtime = LogTime(file,Files,newtime) if Debug==True else 0
+			for userloc in locateAll(file, im, confidence=0.9, region=Cords):
+				x, y = center(userloc)
+				if not 'Assist' in file:
+					TeamColor = GenTeamColor(im, x-35, y)
+					red, green, blue = im.getpixel((int(x)-70, int(y)))
+					if red > 250 and green > 245 and      blue > 240 or \
+					   red > 250 and green < 20  and 30 < blue < 50:
+						name = file.replace(KFPath,'').replace('.png','')
+						if TeamColor == 'Blue':
+							TrackDeath(name,TeamColor,Sub,Dom,ListOfCycles,c_killfeed)
+				else:
+					TeamColor = GenTeamColor(im, x-15, y)
+					name = file.replace(KFPath,'').replace('.png','')
+					print(Sub,name)
+					if Sub in name and TeamColor == 'Blue':
+						TreatSub('Assist',name,ListOfCycles,c_killfeed)
+	except KeyboardInterrupt:
+		pass
+
+###########################################
+# ####################################### #
+###########################################	
+
+def GenCycles():
+	GamesPath = 'Resources\\Healslut Games\\Overwatch '
+	with open(GamesPath+'DeathSub.txt', 'r') as f:
+		SubCycle = cycle(f.readlines())
+	with open(GamesPath+'DeathDom.txt', 'r') as f:
+		DomCycle = cycle(f.readlines())
+	with open(GamesPath+'DeathTeam.txt', 'r') as f:
+		TeamCycle = cycle(f.readlines())
+	with open(GamesPath+'DeathAssist.txt', 'r') as f:
+		AssistCycle = cycle(f.readlines())
+	with open(GamesPath+'DeathKill.txt', 'r') as f:
+		KillsCycle = cycle(f.readlines())
+	return [SubCycle,DomCycle,TeamCycle,AssistCycle,KillsCycle]
+
+def GenCords(self,screen_x=0,screen_y=0):	#Imported
+	screen_x = self.master.winfo_screenwidth() if screen_x == 0 else screen_x
+	screen_y = self.master.winfo_screenheight() if screen_y == 0 else screen_y
+	StartXPos = int(screen_x*.75)
+	EndXPos = int(screen_x*.25)
+	EndYPos = int(screen_y*.25)
+	return (StartXPos,0, EndXPos, EndYPos)
+
+def LogTime(file,Files,newtime):	#Debug
+	if file == Files[0]:
+		lasttime = time()
+		print(lasttime-newtime)
+		newtime = lasttime		
+	return newtime
+
+def RunTest():	#Debug
+	import multiprocessing as mp
+	from pyautogui import screenshot
+	p_killfeed, c_killfeed = mp.Pipe()
+	ListOfCycles = GenCycles()
+	Cords = GenCords('',2560,1440)
+	Freeplay=False
+	Sub = 'Mercy'
+	Dom = 'Ana'
+	KFPath = 'Resources\\Killfeed\\Overwatch\\'
+	Files = glob(KFPath+'*.png') if Freeplay == True else \
+				[KFPath+Sub+'.png', KFPath+Dom+'.png']
+	while True:
+		sleep(5)
+		im = screenshot()
+		Main(im,Files,Sub,Dom,Cords,ListOfCycles,c_killfeed,KFPath)
+		if p_killfeed.poll() == True:
+			print(p_killfeed.recv())
+
+if __name__ == '__main__':
+	RunTest()
