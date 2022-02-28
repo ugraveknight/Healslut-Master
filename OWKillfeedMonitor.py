f049fc01ad4af5ec5289bff28f0528d89a08ba14
uGraveknight1 <45131023+ugraveknight@users.noreply.github.com>
2022-02-27 21:06:30 -0500

    2.0.0

--- OWKillfeedMonitor.py
+++ OWKillfeedMonitor.py
@@ -2,41 +2,112 @@ from pyautogui import locateAll, center
 from glob import glob
 from itertools import cycle
 from time import sleep, time
+from timeit import default_timer as timer
+
+HSDEBUG = False
+#HSDEBUG = True
 	
-def TreatSub(info,name,c_killfeed):
-	c_killfeed.send(info)
-	
-def TrackDeath(name,TeamColor,Sub,Dom,c_killfeed):
-	print('Death:',TeamColor,name)
-	TreatStr = 'DomDeath'  if name == Dom.replace('Resources\\Killfeed\\Overwatch\\','').replace('.png','') and TeamColor == 'Blue' else \
-			   'SubDeath'  if name == Sub else \
-			   'TeamDeath' if TeamColor == 'Blue' else 'Kill'
-	if TeamColor == 'Blue':
-		TreatSub(TreatStr,name,c_killfeed) if TreatStr != '' else 0
+def TreatSub(Mark,TreatStr,Log,c_killfeed):
+	c_killfeed.send((Mark,TreatStr,Log))
 	
 def GenTeamColor(im, x, y):
 	BorderRed,_,BorderBlue = im.getpixel((int(x), int(y)))
-	return 'Red' if BorderRed > BorderBlue else 'Blue'
+	if BorderRed > 250 and BorderBlue < 250:
+		return 'Red'
+	elif BorderRed < BorderBlue:
+		return 'Blue'
+	else:
+		return 'Red'
 
-def Main(im,Files,Sub,Dom,Cords,c_killfeed,KFPath,BorderPixels,Debug=False):
+def FindActivity(im,Files,Sub,Dom,Cords,c_killfeed,KFPath,BorderPixels):
+	FoundDict = {}
+	for file in Files:
+		for userloc in locateAll(file, im, confidence=0.9, region=Cords):
+			x, y = center(userloc)
+			if not 'Assist' in file:
+				TeamColor = GenTeamColor(im, x-BorderPixels[1], y)
+				name = file.replace(KFPath,'').replace('.png','')
+				FoundDict[TeamColor+'|'+name+'|'+str(x)] = (x,y,timer())
+			else:
+				TeamColor = GenTeamColor(im, x-BorderPixels[0], y)	#BorderPixels = 15 on 2560
+				name = file.replace(KFPath,'').replace('.png','')
+				TreatStr = 'Assist'
+				if Sub in name and TeamColor == 'Blue':
+					Log = '%s got an assist!'%(TeamColor+'|'+name)
+					TreatSub(timer(),TreatStr,Log,c_killfeed)
+	return FoundDict
+	
+def Main(im,Files,Sub,Dom,Cords,c_killfeed,KFPath,BorderPixels):
 	try:
-		newtime = time() if Debug==True else 0
-		for file in Files:
-			newtime = LogTime(file,Files,newtime) if Debug==True else 0
-			for userloc in locateAll(file, im, confidence=0.9, region=Cords):
-				x, y = center(userloc)
-				if not 'Assist' in file:
-					TeamColor = GenTeamColor(im, x-BorderPixels[1], y)
-					red, green, blue = im.getpixel((int(x)-BorderPixels[2], int(y)))
-					if red > 250 and green > 245 and      blue > 240 or \
-					   red > 250 and green < 20  and 30 < blue < 50:
-						name = file.replace(KFPath,'').replace('.png','')
-						TrackDeath(name,TeamColor,Sub,Dom,c_killfeed)
+		FoundDict = FindActivity(im,Files,Sub,Dom,Cords,c_killfeed,KFPath,BorderPixels)
+		
+		Output = []
+		for Tup,xy in FoundDict.items():
+			TeamColor, name, _ = Tup.split('|')
+			Tup = Tup.replace('|'+_,'')
+			x,y,Mark = xy
+			for nTup,nxy in FoundDict.items():
+				nTeamColor, nname, _ = nTup.split('|')
+				nTup = nTup.replace('|'+_,'')
+				if TeamColor != nTeamColor:
+					nx,ny,_ = nxy
+					
+					if y-2 < ny < y+2 and x < nx:
+						if TeamColor == 'Blue':
+							if name == Sub:
+								Log = '%s killed %s'%(Tup,nTup)
+								TreatStr = 'Kill'
+							else:
+								Log = '%s, not you, killed %s'%(Tup,nTup)
+								TreatStr = 'TeamKill'
+						
+						else:
+							if nname == Sub:
+								Log = 'You were killed by %s'%Tup
+								TreatStr = 'SubDeath'
+							elif nname == Dom:
+								Log = 'Your Dom, Blue|%s was killed by %s'%(Dom,Tup)
+								TreatStr = 'DomDeath'
+							else:
+								Log = 'Your Teammate, Blue|%s was killed by %s'%(nname,Tup)
+								TreatStr = 'TeamDeath'
+						Output.append((Mark,TreatStr,Log))
+				elif Tup != nTup and TeamColor == 'Blue':
+					if   name  == 'Mercy':
+						Log = '%s Ressurected %s'%(Tup,nTup)
+						TreatStr = 'Ressurect'
+						Output.append((Mark,TreatStr,Log))
+					elif nname == 'Mercy':
+						Log = '%s Ressurected %s'%(nTup,Tup)
+						TreatStr = 'Ressurect'
+						Output.append((Mark,TreatStr,Log))
+			
+				# look this code here is designed to capture player suicide, but it
+				#  sometimes catches the killfeed dropping out and throws false positives
+				#  for the health of the game and for my own mental health, I'm only going
+				#  to act on logs that we can prove happened such as:
+				#		Red|Tracer kills Blue|Mercy
+			if len(FoundDict) == 1 and False:
+				# We go in here and double check this twice.
+				sleep(.2)
+				if len(FindActivity(im,Files,Sub,Dom,Cords,c_killfeed,KFPath,BorderPixels)) == 1:
+					sleep(.2)
+					if len(FindActivity(im,Files,Sub,Dom,Cords,c_killfeed,KFPath,BorderPixels)) == 1:
+					
+						Log = '%s commited suicide'%Tup
+						if TeamColor == 'Blue':
+							if name == Sub:
+								TreatStr = 'SubDeath'
+							else:
+								TreatStr = 'TeamDeath'
+							Output.append((Mark,TreatStr,Log))
+					else:
+						print('That was a close call, potentially another false positive')
 				else:
-					TeamColor = GenTeamColor(im, x-BorderPixels[0], y)	#BorderPixels = 15 on 2560
-					name = file.replace(KFPath,'').replace('.png','')
-					if Sub in name and TeamColor == 'Blue':
-						TreatSub('Assist',name,c_killfeed)
+					print('That was a false positive')
+			
+		for Mark,TreatStr,Log in Output:
+			TreatSub(Mark,TreatStr,Log,c_killfeed)
 	except KeyboardInterrupt:
 		pass
 
@@ -50,15 +121,24 @@ def GenCords(self,screen_x=0,screen_y=0):	#Imported
 	StartXPos = int(screen_x * .75)
 	EndXPos = int(screen_x * .25)
 	EndYPos = int(screen_y * .25)
+	
 	KillFeedBorderPixels = [int(screen_x * .005859375),
 							int(screen_x * .01367875),
 							int(screen_x * .02734375)]
+	if screen_x == 3440:
+		KillFeedBorderPixels = [int(screen_x * .005859375),
+							int(35),
+							int(70)]
+	
+	#width=800
+	#height=800
+	#relx=.75, rely=.25
 	return (StartXPos,0, EndXPos, EndYPos), KillFeedBorderPixels
 
 def LogTime(file,Files,newtime):	#Debug
 	if file == Files[0]:
 		lasttime = time()
-		print(lasttime-newtime)
+		#print(lasttime-newtime)
 		newtime = lasttime		
 	return newtime
 
